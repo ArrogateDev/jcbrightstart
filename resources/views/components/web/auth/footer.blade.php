eb263373fe3bbc7301a4c2315311d7c647c87d5d
ARROGATE-MENG\novms <dev@arrogate.hk>
2025-12-17 15:57:59 +0800

    20251217

---
+++ resources/views/components/web/auth/footer.blade.php
@@ -0,0 +1,106 @@
+<!-- Footer -->
+<footer class="footer footer-one">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row row-gap-4">
+                <div class="col-lg-4">
+                    <div class="footer-about">
+                        <div class="footer-logo">
+                            <img class="logo-max-200" src="{{web_resource_url('assets/admin/img/logo-black.png')}}" alt="">
+                        </div>
+                        <p>Platform designed to help organizations, educators, and learners manage, deliver, and
+                            track learning and training activities.</p>
+                        <div class="d-flex align-items-center d-none">
+                            <a href="#" class="me-2"><img src="{{web_resource_url('assets/admin/img/icon/appstore.svg')}}" alt=""></a>
+                            <a href="#"><img src="{{web_resource_url('assets/admin/img/icon/googleplay.svg')}}" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="row row-gap-4">
+                        <div class="col-lg-4 col-md-4 d-none">
+                            <div class="footer-widget footer-menu">
+                                <h5 class="footer-title">Support</h5>
+                                <ul>
+                                    <li><a href="course-grid.html">Education</a></li>
+                                    <li><a href="add-course.html">Enroll Course</a></li>
+                                    <li><a href="javscript:void(0);">Orders</a></li>
+                                    <li><a href="pricing-plan.html">Payments</a></li>
+                                    <li><a href="blog-grid.html">Blogs</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4">
+                            <div class="footer-widget footer-menu">
+                                <h5 class="footer-title">About</h5>
+                                <ul>
+                                    <li class=" d-none"><a href="course-category.html">Categories</a></li>
+                                    <li class=" d-none"><a href="course-list.html">Courses</a></li>
+                                    <li><a href="{{route('page', ['page' => 'about-us.html'])}}">About Us</a></li>
+                                    <li><a href="{{route('page', ['page' => 'faq.html'])}}"">Faq</a></li>
+                                    <li><a href="{{route('page', ['page' => 'contact-us.html'])}}">Contacts</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 d-none">
+                            <div class="footer-widget footer-menu">
+                                <h5 class="footer-title">Useful Links</h5>
+                                <ul>
+                                    <li><a href="javascript:void(0);">Our values</a></li>
+                                    <li><a href="javascript:void(0);">Our advisory board</a></li>
+                                    <li><a href="javascript:void(0);">Our partners</a></li>
+                                    <li><a href="javascript:void(0);">Become a partner</a></li>
+                                    <li><a href="javascript:void(0);">Work at Future Learn</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 d-none">
+                    <div class="footer-widget footer-contact">
+                        <h5 class="footer-title">Subscribe Newsletter</h5>
+                        <div class="footer-newsletter">
+                            <p>Sign up to get updates & news.</p>
+                            <form action="javascript:void(0);">
+                                <div class="subscribe-form">
+											<span>
+												<i class="isax isax-message-text"></i>
+											</span>
+                                    <input type="email" class="form-control" placeholder="Email Address">
+                                </div>
+                                <button type="submit" class="btn btn-secondary btn-xl w-100">Subscribe</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row row-gap-2">
+                <div class="col-lg-5">
+                    <div class="text-center text-lg-start">
+                        <p>Copyright 2025 © <span class="text-secondary">Jockey Club Bright Start Project</span>. All
+                            right reserved.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <ul class="d-flex align-items-center justify-content-center footer-link">
+                        <li><a href="{{route('page', ['page' => 'terms-and-conditions.html'])}}">Terms & Conditions</a></li>
+                        <li><a href="{{route('page', ['page' => 'privacy-policy.html'])}}">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <div class="social-icon">
+                        <a href="javascript:void(0);"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="javascript:void(0);"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="javascript:void(0);"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a href="javascript:void(0);"><i class="fa-brands fa-youtube"></i></a>
+                        <a href="javascript:void(0);"><i class="fa-brands fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/components/web/auth/head.blade.php
@@ -0,0 +1,99 @@
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+          content="Arrogate LMS is a powerful Learning Management System template designed for educators, training institutions, and businesses. Manage courses, track student progress, conduct virtual classes, and enhance e-learning experiences with an intuitive and feature-rich platform.">
+    <meta name="keywords"
+          content="LMS template, Learning Management System, e-learning software, online course platform, student management, education portal, virtual classroom, training management system, course tracking, online education">
+    <meta name="author" content="Dreams Technologies">
+    <meta name="robots" content="index, follow">
+
+    <title>Arrogate LMS | Advanced Learning Management System Template</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{web_resource_url('assets/admin/img/favicon.ico')}}'">
+    <link rel="apple-touch-icon" href="{{web_resource_url('assets/admin/img/favicon.ico')}}">
+
+    <!-- Theme Settings Js -->
+    <script src="{{web_resource_url('assets/admin/js/theme-script.js')}}" type="text/javascript"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/css/bootstrap.min.css')}}">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/swiper/css/swiper-bundle.min.css')}}">
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/fontawesome/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/fontawesome/css/all.min.css')}}">
+
+    <!-- Select2 CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/select2/css/select2.min.css')}}">
+
+    <!-- Slick CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/slick/slick.css')}}">
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/slick/slick-theme.css')}}">
+
+    <!-- Feathericon CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/feather/feather.css')}}">
+
+    <!-- Aos CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/aos/aos.css')}}">
+
+    <!-- Tabler Icon CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/tabler-icons/tabler-icons.css')}}">
+
+    <!-- Iconsax CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/css/iconsax.css')}}">
+
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/css/owl.theme.default.min.css')}}">
+
+    <!-- Fancybox CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/fancybox/jquery.fancybox.min.css')}}">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/css/style.css')}}">
+
+    <!-- jQuery -->
+    <script src="{{web_resource_url('assets/admin/js/jquery-3.7.1.min.js')}}" type="text/javascript"></script>
+
+    <!-- Bootstrap Core JS -->
+    <script src="{{web_resource_url('assets/admin/js/bootstrap.bundle.min.js')}}" type="text/javascript"></script>
+
+    <!-- Select2 JS -->
+    <script src="{{web_resource_url('assets/admin/plugins/select2/js/select2.min.js')}}" type="text/javascript"></script>
+
+    <!-- Slick Slider -->
+    <script src="{{web_resource_url('assets/admin/plugins/slick/slick.js')}}" type="text/javascript"></script>
+
+    <!-- Swiper Slider -->
+    <script src="{{web_resource_url('assets/admin/plugins/swiper/js/swiper-bundle.min.js')}}" type="text/javascript"></script>
+
+    <!-- counterup JS -->
+    <script src="{{web_resource_url('assets/admin/js/counter.js')}}" type="text/javascript"></script>
+    <script src="{{web_resource_url('assets/admin/js/jquery.waypoints.js')}}" type="text/javascript"></script>
+    <script src="{{web_resource_url('assets/admin/js/jquery.counterup.min.js')}}" type="text/javascript"></script>
+
+    <!-- Owl Carousel -->
+    <script src="{{web_resource_url('assets/admin/js/owl.carousel.min.js')}}" type="text/javascript"></script>
+
+    <!-- Aos -->
+    <script src="{{web_resource_url('assets/admin/plugins/aos/aos.js')}}" type="text/javascript"></script>
+
+    <!-- Fancybox JS -->
+    <script src="{{web_resource_url('assets/admin/plugins/fancybox/jquery.fancybox.min.js')}}" type="text/javascript"></script>
+
+    <!-- Custom JS -->
+    <script src="{{web_resource_url('assets/admin/js/script.js')}}" type="text/javascript"></script>
+
+    <script type="text/javascript" src="{{web_resource_url('assets/admin/js/lodash.js') }}"></script>
+    <script src="{{web_resource_url('assets/admin/plugins/wait-me/waitMe.min.js')}}" type="text/javascript"></script>
+    <link rel="stylesheet" href="{{web_resource_url('assets/admin/plugins/wait-me/waitMe.min.css')}}">
+    <link href="{{web_resource_url('assets/admin/plugins/toastr/toastr.min.css')}}" rel="stylesheet"/>
+    <script src="{{web_resource_url('assets/admin/plugins/toastr/toastr.min.js')}}"></script>
+    <script type="text/javascript" src="{{web_resource_url('assets/admin/js/utils.js') }}"></script>
+</head>
