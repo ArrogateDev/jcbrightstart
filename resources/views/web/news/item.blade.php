a34c7919975be403eb03bc24d43c2ac7db922984
ARROGATE-MENG\novms <dev@arrogate.hk>
2026-06-05 18:09:32 +0800

    20260605

--- resources/views/web/news/item.blade.php
+++ resources/views/web/news/item.blade.php
@@ -6,7 +6,7 @@
             <div class="news-item-box-body bg-[#fff] p-[45px] rounded-b-lg">
                 <div class="flex gap-[20px]">
                     <div class="title border-t-[1px] border-[#d1c7c0] grow title-tooltip pt-[17px]" data-tooltip="{{$news->release_date}}">
-                        <span class="h-[45px] text-[24px] font-bold leading-9 line-clamp-4">{{$news->title}}</span>
+                        <span class="h-[145px] text-[24px] font-bold leading-9 line-clamp-4">{{$news->title}}</span>
                     </div>
                     <div class="w-[203px] h-[153px] rounded-lg flex-none overflow-hidden">
                         <img class="w-full rounded-lg" src="{{$news->thumbnail}}" alt="{{$news->title}}">
