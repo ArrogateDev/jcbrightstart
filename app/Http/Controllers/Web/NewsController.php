2d25312d4bc57aa1d1be68c6b78c1184bea9da29
ARROGATE-MENG\novms <dev@arrogate.hk>
2026-06-10 11:16:55 +0800

    20260610

--- app/Http/Controllers/Web/NewsController.php
+++ app/Http/Controllers/Web/NewsController.php
@@ -60,7 +60,6 @@ class NewsController extends Controller
         $keywords = $request->query('keywords');
         $category = (int)$request->query('category');
         $type = (int)$request->query('type', 0);
-        $now = Carbon::now()->toDateString();
         $params = [
             'keywords' => $keywords,
             'category' => $category,
@@ -75,11 +74,6 @@ class NewsController extends Controller
                 $query->where('category_id', $category);
             })
             ->where('type', $type)
-//            ->when($type === 1, function ($query) use ($now) {
-//                $query->where('release_date', '>=', $now);
-//            }, function ($query) use ($now) {
-//                $query->where('release_date', '<', $now);
-//            })
             ->where('status', News::STATUS_PUBLISHED)
             ->orderByDesc('sort')
             ->orderByDesc('id')
@@ -114,6 +108,7 @@ class NewsController extends Controller
      */
     public function show(News $news, Request $request)
     {
+        $type = (int)$request->query('type', 0);
         $breadcrumbs = [
             [
                 'title' => __('最新消息'),
@@ -132,17 +127,31 @@ class NewsController extends Controller
         $news->day = $date->format('d');
 
         $prev = News::query()
-            ->where('id', '<', $news->id)
+            ->where('type', $type)
             ->where('status', News::STATUS_PUBLISHED)
-            ->orderByDesc('sort')
-            ->orderBy('id', 'desc')
+            ->where(function ($query) use ($news) {
+                $query->where('sort', '>', $news->sort)
+                    ->orWhere(function ($query) use ($news) {
+                        $query->where('sort', $news->sort)
+                            ->where('id', '>', $news->id);
+                    });
+            })
+            ->orderBy('sort')
+            ->orderBy('id')
             ->value('id');
 
         $next = News::query()
-            ->where('id', '>', $news->id)
+            ->where('type', $type)
             ->where('status', News::STATUS_PUBLISHED)
+            ->where(function ($query) use ($news) {
+                $query->where('sort', '<', $news->sort)
+                    ->orWhere(function ($query) use ($news) {
+                        $query->where('sort', $news->sort)
+                            ->where('id', '<', $news->id);
+                    });
+            })
             ->orderByDesc('sort')
-            ->orderBy('id', 'asc')
+            ->orderByDesc('id')
             ->value('id');
 
         $url = $request->session()->get('news-url');
