be7effe95ed4ae6877b9f12ff47e95ea597da07f
ARROGATE-MENG\novms <dev@arrogate.hk>
2026-09-16 15:48:16 +0800

    20260916

--- resources/views/components/web/header.blade.php
+++ resources/views/components/web/header.blade.php
@@ -6,7 +6,7 @@
                     <img class="w-[185px] h-[81px]" src="{{web_resource_url('assets/web/images/logo.png')}}" alt="Jockey Club Bright Start Project">
                 </a>
 
-                <div class="hidden xl:flex items-center gap-x-[33px]">
+                <div class="hidden 2xl:flex items-center gap-x-[33px]">
                     @if(!empty($navs))
                         <nav>
                             <ul class="flex gap-x-[35px]">
@@ -35,30 +35,11 @@
                     @endif
                     <div class="flex flex-col gap-y-3">
                         @auth
-                            <div class="dropdown dropdown-hover">
-                                <div tabindex="0" role="button" class="flex items-center gap-x-2 text-[19px] text-[#998675] font-bold">
-                                    <div tabindex="0" role="button" class="w-10 h-10 bg-white rounded-full">
-                                        <img src="{{$user->avatar}}" alt="Img" class="img-fluid rounded-full">
-                                    </div>
-                                    <h6>{{$user->full_name}}</h6>
-                                </div>
-                                <ul tabindex="-1" class="dropdown-content menu bg-[#ffffffcc] rounded-box z-1 w-52 p-2 shadow-sm">
-                                    @foreach($avatar_menus as $menu)
-                                        <li>
-                                            <a class="flex align-center gap-x-3 text-[19px] text-[#998675] font-bold" href="{{$menu['url']}}" target="{{$menu['target']??'_self'}}">
-                                                <img class="w-[20px]" src="{{web_resource_url('assets/web/images/arrow.svg')}}" alt="{{$menu['title']}}">
-                                                <span>{{$menu['title']}}</span>
-                                            </a>
-                                        </li>
-                                    @endforeach
-                                    <div class="divider"></div>
-                                    <li>
-                                        <a href="#" class="flex align-center gap-x-3 text-[19px] text-[#998675] font-bold logout">
-                                            <img class="w-[20px]" src="{{web_resource_url('assets/web/images/arrow.svg')}}" alt="{{__('退出登录')}}">
-                                            <span>{{__('退出登录')}}</span>
-                                        </a>
-                                    </li>
-                                </ul>
+                            <div class="bg-[#43c8d5] px-[45px] rounded-lg">
+                                <a class="flex justify-center gap-x-1 text-white text-[21px] font-bold" href="{{route('user.dashboard.html')}}">
+                                    <img class="w-[20px]" src="{{web_resource_url('assets/web/images/login.svg')}}" alt="">
+                                    {{__('家长登入')}}
+                                </a>
                             </div>
                         @else
                             <div class="bg-[#43c8d5] px-[45px] rounded-lg">
@@ -71,7 +52,7 @@
                     </div>
                 </div>
 
-                <div class="drawer drawer-end xl:hidden w-auto">
+                <div class="drawer drawer-end 2xl:hidden w-auto">
                     <input id="header-drawer" type="checkbox" class="drawer-toggle"/>
                     <div class="drawer-content flex items-center">
                         <label for="header-drawer" class="btn btn-ghost btn-circle">
